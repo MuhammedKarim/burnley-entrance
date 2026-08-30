fdeab77db702c7023742dcfcb5c6a4a0e09b7432
Muhammed Karim <muhammedkarim@hotmail.co.uk>
2026-08-30 01:24:56 +0100

    switch prayer times control

--- script.js
+++ script.js
@@ -455,12 +455,15 @@ function initPrayerTimes() {
   }
 
   function fetchPrayerTimes() {
-    fetch(`prayer-times.json?t=${Date.now()}`)
+    fetch(`https://muhammedkarim.github.io/burnley-prayers/prayer-times.json?t=${Date.now()}`)
       .then(res => res.json())
       .then(data => {
         allData = data;
         loadPrayerTimes();
         updateClock();
+      })
+      .catch(error => {
+        console.error('Failed to fetch prayer times:', error);
       });
   }
     
